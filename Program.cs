bd94595b7116aab75c090261b3c5019229c6fd61
Logan Harris <logan.harris41@yahoo.com>
2021-09-29 16:19:48 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PowersTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runProgram = true;
+
+            Console.WriteLine("Learn your squares and cubes!\n");
+
+            while (runProgram == true)
+            {
+                chartTable();
+                Console.WriteLine("");
+
+                Console.WriteLine("Continue? (y/n)");
+                string loopAnswer = Console.ReadLine();
+                if (loopAnswer == "n")
+                {
+                    runProgram = false;
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public static void chartTable()
+        { 
+            Console.WriteLine("Enter an integer: ");
+            int numInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            if (numInput <= 0 || numInput >= 1290)
+            {
+                Console.WriteLine($"{numInput} is not vaid input.");
+            }
+
+            Console.WriteLine("Number\tSquared\tCubed");
+            Console.WriteLine("=====\t=====\t=====");
+            
+            for (int i = 1; i <= numInput; i++)
+            //Max Int is 1290
+            {
+                int square = i * i;
+                int cube = square * i;
+                Console.WriteLine($"{i} \t {square} \t {cube}");
+            }
+            
+            
+        }
+    }
+}
